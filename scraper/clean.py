cd2c6a51b74d808d5d5e65a9dafd71961d9e1f5a
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-02-23 15:32:36 -0800

    cleaning the socrata data

--- scraper/clean.py
+++ scraper/clean.py
@@ -3,7 +3,7 @@ Input 1042196 rows
 Output 1032442 rows
 """
 
-with open('clean.csv', 'r') as csvinput:
+with open('seattle911_2011_2015_socrata.csv', 'r') as csvinput:
     firstline = csvinput.readline()
     temp_list = csvinput.readlines()
 
@@ -12,6 +12,6 @@ print 'length pre set: {}'.format(len(temp_list))
 set_list = set(temp_list)
 print 'length post-set: {}'.format(len(set_list))
 
-with open('clean.csv', 'wb') as output:
+with open('clean_socrata.csv', 'wb') as output:
     output.write(firstline)
     output.writelines(set_list)
