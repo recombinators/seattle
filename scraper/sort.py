2f1a9bfd45e4260b320af1d1e5e9c4c21cba5f5e
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-23 13:50:51 -0800

    #9 date sorted.

--- scraper/sort.py
+++ scraper/sort.py
@@ -3,7 +3,7 @@ import pprint
 import dateutil.parser
 import datetime
 
-with open('clean.csv', 'r') as csvinput:
+with open('clean_addr.csv', 'r') as csvinput:
     firstline = csvinput.readline()
     temp_list = csvinput.readlines()
 
@@ -11,17 +11,20 @@ with open('clean.csv', 'r') as csvinput:
     data_bad = []
     for line in temp_list:
         line_split = line.split(',')
-        # pprint.pprint(line_split[1])
-        try:
-            line_split[1] = datetime.datetime.isoformat(dateutil.parser.parse(line_split[1]))
-        except ValueError:
-            data_bad.append(line_split)
 
-        # pprint.pprint(line_split[1])
+        try:
+            line_split[1] = datetime.datetime.isoformat(
+                dateutil.parser.parse(line_split[1]))
+        except ValueError:
+            pass
+
         data_list_of_lists.append(line_split)
 
     data_sorted = sorted(data_list_of_lists, key=itemgetter(1))
+    for i, line in enumerate(data_sorted):
+        data_sorted[i] = ','.join(line)
 
-    data_sorted_print = data_sorted[:10]
 
-    pprint.pprint(data_bad)
+with open('clean_date.csv', 'wb') as output:
+    output.write(firstline)
+    output.writelines(data_sorted)
