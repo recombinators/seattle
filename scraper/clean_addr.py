548c7c1ddd8db81e2bbeaae21661e808c8aa8c6a
Mark Saiget <saiget@gmail.com>
2015-02-23 13:20:22 -0800

    Complete clean_addr.py, so that addresses containing / are replaced with and. clean_addr.py contains the output of this.

--- scraper/clean_addr.py
+++ scraper/clean_addr.py
@@ -6,27 +6,25 @@ import csv
 from string import replace
 
 with open('clean.csv', 'r') as csvinput:
-    firstline = csvinput.readline()
     temp_list = csv.reader(csvinput)
 
     new_list = []
     for line in temp_list:
         try:
             if "/" in line[3]:
-                line = replace(line[3], '/', 'and')
+                line[3] = replace(line[3], '/', 'and')
             new_list.append(line)
         except IndexError:
             continue
 
 
 with open('clean_addr.csv', 'wb') as output:
-    #csvwriter = csv.writer(output)
-    #csvwriter.writerow([firstline])
-    #csvwriter.writerows([new_list])
-    output.write(firstline)
+    csvwriter = csv.writer(output)
+    csvwriter.writerows(new_list)
+    # output.write(firstline)
     
-    import pdb; pdb.set_trace()
+    # # import pdb; pdb.set_trace()
     
         
     
-    output.writelines([new_list])
+    # output.writelines(new_list)
