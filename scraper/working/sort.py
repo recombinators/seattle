420e75715de94bb738e85ca71df90d2962fdfa0d
Mark Saiget <saiget@gmail.com>
2015-02-23 17:50:41 -0800

    Remove files moved to testing folder.

--- scraper/working/sort.py
+++ scraper/working/sort.py
@@ -1,27 +1,13 @@
+"""This sorts data based on lat and long."""
+
 from operator import itemgetter
 # import dateutil.parser
 import csv
 
 with open('mapped.csv', 'r') as csvinput:
-    # firstline = csvinput.readline()
     temp_list = csv.reader(csvinput)
 
-    # data_list_of_lists = []
-    # data_bad = []
-    # for line in temp_list:
-    #     line_split = line.split(',')
-
-    #     # try:
-    #     #     line_split[1] = datetime.datetime.isoformat(
-    #     #         dateutil.parser.parse(line_split[1]))
-    #     # except ValueError:
-    #     #     pass
-
-    #     data_list_of_lists.append(line_split)
-
     data_sorted = sorted(temp_list, key=itemgetter(6))
-    # for i, line in enumerate(data_sorted):
-    #     data_sorted[i] = ','.join(line)
 
 
 with open('mapped_sort.csv', 'wb') as output:
--- scraper/working/t_geocode.py
+++ scraper/working/t_geocode.py
@@ -8,6 +8,7 @@ with open('clean_addr_socrata.csv', 'r') as csvinput:
     dict_ = {}
     for x in temp_list:
         dict_.setdefault(x[0], [x[3], x[4]])
+        print dict_[x[0]]
     print 'length dict: {}'.format(len(dict_))
 
 with open('clean_addr.csv', 'r') as csvinput2:
@@ -19,14 +20,11 @@ with open('clean_addr.csv', 'r') as csvinput2:
         lon = 0
         if line[3] in dict_:
             lat = dict_[line[3]][0]
-            lon = dict_[line[3]][1]
+            lon = dict_[line[3]][0]
         line.append(lat)
         line.append(lon)
         new_list.append(line)
 
-
-# set_list = set(output)
-# print 'length post-set: {}'.format(len(set_list))
 
 with open('mapped.csv', 'wb') as output:
     csvwriter = csv.writer(output)
