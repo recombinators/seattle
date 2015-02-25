fccfee53cccaab57a493cc1e9abf28d12cfedd3d
Mark Saiget <saiget@gmail.com>
2015-02-24 16:40:25 -0800

    Add MVP view to routes.

--- seattle/seattle/__init__.py
+++ seattle/seattle/__init__.py
@@ -19,5 +19,6 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('one_hundred', '/100')
+    config.add_route('MVP', '/mvp')
     config.scan()
     return config.make_wsgi_app()
