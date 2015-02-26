345a9fd6e5708c2234ed39d477478cdb2ba1e39a
jacquestardie <hi@jacquestardie.org>
2015-02-25 22:52:22 -0800

    map.getCenter()

--- seattle/seattle/static/js/map.js
+++ seattle/seattle/static/js/map.js
@@ -1,7 +1,14 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiamFjcXVlcyIsImEiOiJuRm9TWGYwIn0.ndryRT8IT0U94pHV6o0yng';
+
 // Initialize map
-var map = L.mapbox.map('map', 'jacques.la14ofjk', {zoomControl: false });
-// Set center and zoom
-map.setView([47.6105411, -122.329726], 13);
-// Create lat/lon url hash
-var hash = L.hash(map);
+var map = L.mapbox.map('map', 'jacques.la14ofjk', {
+    center: [47.6105411, -122.329726],
+    zoom: 13,
+    zoomControl: false,
+    attributionControl: false
+});
+
+// On move, recalculate center
+map.on('moveend', function(e) {
+    console.log(map.getCenter());
+});
