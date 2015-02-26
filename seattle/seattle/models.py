e447289aa0700dbed956344e147b284127d8cb89
Mark Saiget <saiget@gmail.com>
2015-02-25 20:22:45 -0800

    Clean up code and add lat long view for testing.

--- seattle/seattle/models.py
+++ seattle/seattle/models.py
@@ -1,5 +1,4 @@
 from sqlalchemy import (
-    Column,
     Index,
     Integer,
     Text,
--- seattle/seattle/views.py
+++ seattle/seattle/views.py
@@ -1,10 +1,6 @@
 from pyramid.response import Response
 from pyramid.view import view_config
-
 from sqlalchemy.exc import DBAPIError
-from sqlalchemy import func
-
-
 from .models import (
     DBSession,
     Incidents_Model,
@@ -12,7 +8,6 @@ from .models import (
 
 import time
 import pytz
-import datetime
 import math
 
 
@@ -43,58 +38,72 @@ def convert_json(query):
     return list_output
 
 
-# @view_config(route_name='index', renderer='templates/index.jinja2')
-# def my_view(request):
+# @view_config(route_name='center', renderer='json')
+# def center(request):
+#     "Returns lat/lon params"
+#     lat = request.matchdict.get('lat', None)
+#     lon = request.matchdict.get('lon', None)
 #     try:
-#         one = DBSession.query(Incidents_Model).filter(Incidents_Model.gid == 378).first()
+#         output = DBSession.query(Incidents_Model).filter(func.ST_Point_Inside_Circle(Incidents_Model.the_geom, lon, lat, 0.001))
+#         # print 'query: {}\ncount: {}'.format(output, output.count())
 #     except DBAPIError:
 #         return Response(conn_err_msg, content_type='text/plain', status_int=500)
-#     return {'one': one, 'project': 'seattle'}
+#     # Convert sqlalchemy object into list of dictionaries.
+#     return {'output': convert_json(output)}
 
-
-@view_config(route_name='center', renderer='json')
-def center(request):
-    "Returns lat/lon params"
-    lat = request.matchdict.get('lat', None)
-    lon = request.matchdict.get('lon', None)
-    try:
-        output = DBSession.query(Incidents_Model).filter(func.ST_Point_Inside_Circle(Incidents_Model.the_geom, lon, lat, 0.001))
-        # print 'query: {}\ncount: {}'.format(output, output.count())
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    # Convert sqlalchemy object into list of dictionaries.
-    return {'output': convert_json(output)}
-
-
-# @view_config(route_name='index',
-#              renderer='templates/index.jinja2')
-# def mvp_v1(request):
-#     """Returns epoch datetime params as a dict with keys for each major type and
-#     values corresponding to the epoch times."""
-#     lat = 47.609907
-#     lon = -122.337779
-#     radius = 0.005
-#     try:
-#         output = []
-#         output.append(epoch_list(Incidents_Model.cat_circle(lat, lon, 'Fire', radius)))
-#         output.append(epoch_list(Incidents_Model.cat_circle(lat, lon, 'MVI', radius)))
-#         output.append(epoch_list(Incidents_Model.cat_circle(lat, lon, 'Crime', radius)))
-#         # output.append(epoch_list(Incidents_Model.cat_circle(lat, lon, 'Other', radius)))
-#     except DBAPIError:
-#         return Response(conn_err_msg, content_type='text/plain', status_int=500)
-#     names = ['fire', 'mvi', 'crime'] #, 'other'
-#     output_dict = dict(zip(names, output))
-#     output_percentages = []
-#     output_percentages.append(Incidents_Model.percentage(output[0]))
-#     output_percentages.append(Incidents_Model.percentage(output[1]))
-#     output_percentages.append(Incidents_Model.percentage(output[2]))
-#     output_percentages_dict = dict(zip(names, output_percentages))
-
-
-#     return {'lists': output_dict, 'percentages': output_percentages_dict}
 
 @view_config(route_name='index', renderer='templates/index.jinja2')
 def line_plot(request):
+    "Returns epoch datetime params as a list."
+    lat = 47.623636
+    lon = -122.336072
+    radius = 0.003
+    try:
+        output = []
+        start_time = time.time()
+        output.append(epoch_list(Incidents_Model.cat_circle(lat, lon, 'Fire', radius)))
+        output.append(epoch_list(Incidents_Model.cat_circle(lat, lon, 'MVI', radius)))
+        output.append(epoch_list(Incidents_Model.cat_circle(lat, lon, 'Crime', radius)))
+        print 'time to call db for initial query: {}'.format(time.time()-start_time)
+    except DBAPIError:
+        return Response(conn_err_msg, content_type='text/plain', status_int=500)
+
+    names = ['fire', 'mvi', 'crime'] #, 'other'
+    output_dict = dict(zip(names, output))
+    output_percentages = []
+    output_percentages.append(Incidents_Model.percentage(output[0]))
+    output_percentages.append(Incidents_Model.percentage(output[1]))
+    output_percentages.append(Incidents_Model.percentage(output[2]))
+    output_percentages_dict = dict(zip(names, output_percentages))
+
+    min_date = min(min(output[0]), min(output[1]), min(output[2]))
+    max_date = max(max(output[0]), max(output[1]), max(output[2]))
+    date_range = max_date - min_date
+    number_months = int(math.ceil(date_range/30))
+    days_in_month = 30
+    start_date = min_date
+    date = start_date
+
+    months = []
+    for i in range(number_months):
+        months.append(date)
+        date += days_in_month
+    # print(wks)
+
+    count = [[0] * (number_months - 1), [0] * (number_months - 1), [0] * (number_months - 1)]
+    for k in range(2):
+        for i in output[k]:
+            for j in range(number_months - 1):
+                if i > months[j] and i < months[j+1]:
+                    count[k][j] += 1
+    print(count)
+
+    return {'output': [months[1:], count],
+            'percentages': output_percentages_dict}
+
+
+@view_config(route_name='index_lat_long', renderer='templates/index.jinja2')
+def line_plot_lat_long(request):
     "Returns epoch datetime params as a list."
     lat = 47.623636
     lon = -122.336072
