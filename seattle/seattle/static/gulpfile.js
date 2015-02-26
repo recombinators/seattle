bad713f16a41b1cba581955c3e67b66d5fb4b24e
jacquestardie <hi@jacquestardie.org>
2015-02-26 10:54:44 -0800

    update gulp process + document

--- seattle/seattle/static/gulpfile.js
+++ seattle/seattle/static/gulpfile.js
@@ -7,6 +7,7 @@ var fontVariants        = require('postcss-font-variant')
 var customMedia         = require('postcss-custom-media')
 var rename              = require('gulp-rename')
 var minify              = require('gulp-minify-css')
+var util                = require('gulp-util')
 
 
 // Autoprefix and concatenate CSS styles
@@ -18,20 +19,18 @@ gulp.task('build', function () {
         customProperties(),
         customMedia()
     ];
-    return gulp.src('seattle/seattle/static/css/src/index.css')
+    return gulp.src('./css/src/index.css')
         .pipe(postcss(processors))
-        .pipe(gulp.dest('./seattle/seattle/static/css/'))
+        .pipe(gulp.dest('./css/'))
         .pipe(minify())
         .pipe(rename('index.min.css'))
-        .pipe(gulp.dest('seattle/seattle/static/css/'))
+        .pipe(gulp.dest('./css/'))
 })
 
 // Re-run build process on changes to CSS
 gulp.task('watch', function () {
-    gulp.watch('seattle/seattle/css/src/*.css', ['build'])
+    gulp.watch('./css/src/*.css', ['build'])
 })
 
 // Register default task
 gulp.task('default', ['watch'])
-
-// TODO Add: log of output files
