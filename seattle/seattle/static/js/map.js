919b8273d24f1b591fa75d3133b81eadbee7621c
jacquestardie <hi@jacquestardie.org>
2015-02-26 18:03:33 -0800

    add d3-tip

--- seattle/seattle/static/js/map.js
+++ seattle/seattle/static/js/map.js
@@ -18,13 +18,13 @@ var map = L.mapbox.map('map', 'jacques.la14ofjk', {
 map.scrollWheelZoom.disable();
 
 
+// Define stacked bar chart
 function graph() {
     try {
         dates = [];
         for (i = 0; i < bulk_data[0].length; i++) {
             dates.push(new Date(bulk_data[0][i]*1000*60*60*24));
         }
-
 
         data = [];
         for (i = 0; i < bulk_data[0].length; ++i) {
@@ -41,16 +41,23 @@ function graph() {
             p = [50, 40, 40, 20],
             x = d3.scale.ordinal().rangeRoundBands([0, w - p[1] - p[3]]),
             y = d3.scale.linear().range([0, h - p[0] - p[2]]),
-            z = d3.scale.ordinal().range(["lightpink", "darkgray", "lightblue"])
-            yx = d3.scale.linear().range([0, h - p[0] - p[2]]),
-        // parse = d3.time.format("%m/%Y").parse,
-            format = d3.time.format("%b %Y");
+            z = d3.scale.ordinal().range(["#FDE668", "#FFBE1A", "#E09200"])
+            yx = d3.scale.linear().range([0, h - p[0] - p[2]]);
+
+        var tip = d3.tip()
+            .attr('class', 'd3-tip')
+            .offset([-10, 0])
+            .html(function(d) {
+                return "<span>" + d.y + "</span>";
+            })
 
         var svg = d3.select(".graph").append("svg:svg")
             .attr("width", w)
             .attr("height", h)
             .append("svg:g")
             .attr("transform", "translate(" + p[3] + "," + (h - p[2]) + ")");
+
+        svg.call(tip);
 
         // Transpose the data into layers by type.
         var incidents = d3.layout.stack()(["fire", "mvi", "crime"].map(function(incident) {
@@ -68,19 +75,19 @@ function graph() {
         var xAxis = d3.svg.axis()
             .scale(x)
             .orient("bottom")
-            .ticks(5);
+            .ticks(d3.time.year, 5)
+            .tickFormat(d3.time.format('%Y'));
 
         svg.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(" + 0 + "," + h - p[2] + ")")
             .call(xAxis)
             .selectAll("text")
-            .attr("y", 0)
+        //     .attr("y", 0)
             .attr("x", 9)
-            .attr("dy", ".35em")
+        //     // .attr("dy", ".35em")
             .attr("transform", "rotate(90)")
-            .style("text-anchor", "start")
-            .text(format);
+            .style("text-anchor", "start");
 
         var yAxis = d3.svg.axis()
             .scale(yx)
@@ -99,8 +106,7 @@ function graph() {
             .data(incidents)
             .enter().append("svg:g")
             .attr("class", "incident")
-            .style("fill", function(d, i) { return z(i); })
-            .style("stroke", function(d, i) { return d3.rgb(z(i)).darker(); });
+            .style("fill", function(d, i) { return z(i); });
 
         // Add a rect for each date.
         var rect = incident.selectAll("rect")
@@ -109,55 +115,9 @@ function graph() {
             .attr("x", function(d) { return x(d.x); })
             .attr("y", function(d) { return -y(d.y0) - y(d.y); })
             .attr("height", function(d) { return y(d.y); })
-            .attr("width", x.rangeBand());
-
-        var legend_x = (w - p[1]*6),
-            legend_y = 4*h/5;
-
-        svg.append("text")
-            .attr("transform", "translate(" + legend_x + "," + -legend_y + ")")
-            .attr("dy", ".35em")
-            .attr("text-anchor", "start")
-            .style("fill", "lightpink")
-            .text("Fire");
-
-        svg.append("text")
-            .attr("transform", "translate(" + legend_x + "," + -(legend_y + 20) + ")")
-            .attr("dy", ".35em")
-            .attr("text-anchor", "start")
-            .style("fill", "darkgray")
-            .text("Motor Vehicle Incident");
-
-        svg.append("text")
-            .attr("transform", "translate(" + legend_x + "," + -(legend_y + 40) + ")")
-            .attr("dy", ".35em")
-            .attr("text-anchor", "start")
-            .style("fill", "lightblue")
-            .text("Crime");
-
-
-        incident.selectAll("rect")
-        .on("mouseover", function(d){
-            var xPos = parseFloat(d3.select(this).attr("x"));
-            var yPos = parseFloat(d3.select(this).attr("y"));
-            var height = parseFloat(d3.select(this).attr("height"))
-            var coordinatesMouse = d3.mouse(this);
-            var xMouse = coordinatesMouse[0];
-            var yMouse = coordinatesMouse[1];
-
-            d3.select(this)
-                .attr("stroke","blue")
-                .attr("stroke-width",0.8)
-            svg.append("text")
-                .attr("x",xMouse)
-                .attr("y",yMouse)
-                .attr("class","tooltip")
-                .text(d.y);
-        })
-        .on("mouseout",function(){
-            svg.select(".tooltip").remove();
-            d3.select(this).attr("stroke","pink").attr("stroke-width",0.2);
-        })
+            .attr("width", x.rangeBand())
+            .on('mouseover', tip.show)
+            .on('mouseout', tip.hide)
     }
     catch(err) {
         $(".graph").replaceWith('No data available.')
