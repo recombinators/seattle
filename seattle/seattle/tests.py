83688b16c614eb076c99c03dedd448baba338ac9
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-02-27 10:42:51 -0800

    neighborhood test

--- seattle/seattle/tests.py
+++ seattle/seattle/tests.py
@@ -5,7 +5,10 @@
 #from pyramid_sqlalchemy.testing import DatabaseTestCase
 #from pyramid import testing
 
-from models import DBSession, Incidents_Model
+# USE THIS FOR TESTING
+# py.test tests.py --sql-url=postgresql://Joel:@localhost/seattle_test --sql-echo -s
+
+from models import DBSession, Incidents_Model, Neighborhoods_Model
 from views import line_plot, line_plot_lat_long_ajax
 
 import pytest
@@ -14,84 +17,7 @@ import pytest
 TEST_DSN = 'dbname=seattle_test user=Joel'
 
 
-def test_foo(sql_session):
-    import pdb; pdb.set_trace()
-    model = Incidents_Model.by_gid(123, sql_session)
-    assert False
-
-#class ViewIntegrationTests(DatabaseTestCase):
-#    def setUp(self):
-#        super(ViewIntegrationTests, self).setUp()
-#        self.config = testing.setUp()
-#
-#    def test_cat_circle(sql_session):
-#        lat = 47.623636
-#        lon = -122.336072
-#        radius = 0.1
-#        result = Incidents_Model.cat_circle(lat, lon, 'Fire', radius, limit=5)
-#        print result
-#
-#    def tearDown(self):
-#        testing.tearDown()
-#        super(ViewIntegrationTests, self).tearDown()
-#
-#
-#@pytest.fixture
-#def pyramid_req():
-#    return testing.DummyRequest()
-
-
-
-#def test_line_plot_lat_long_returns_correctly(request):
-#    response = line_plot(pyramid_req)
-#    print response
-
-
-
-#class TestMyViewSuccessCondition(unittest.TestCase):
-#    def setUp(self):
-#        self.config = testing.setUp()
-#        from sqlalchemy import create_engine
-#        engine = create_engine('sqlite://')
-#        from .models import (
-#            Base,
-#            MyModel,
-#            )
-#        DBSession.configure(bind=engine)
-#        Base.metadata.create_all(engine)
-#        with transaction.manager:
-#            model = MyModel(name='one', value=55)
-#            DBSession.add(model)
-#
-#    def tearDown(self):
-#        DBSession.remove()
-#        testing.tearDown()
-#
-#    def test_passing_view(self):
-#        from .views import my_view
-#        request = testing.DummyRequest()
-#        info = my_view(request)
-#        self.assertEqual(info['one'].name, 'one')
-#        self.assertEqual(info['project'], 'seattle')
-#
-#
-#class TestMyViewFailureCondition(unittest.TestCase):
-#    def setUp(self):
-#        self.config = testing.setUp()
-#        from sqlalchemy import create_engine
-#        engine = create_engine('sqlite://')
-#        from .models import (
-#            Base,
-#            MyModel,
-#            )
-#        DBSession.configure(bind=engine)
-#
-#    def tearDown(self):
-#        DBSession.remove()
-#        testing.tearDown()
-#
-#    def test_failing_view(self):
-#        from .views import my_view
-#        request = testing.DummyRequest()
-#        info = my_view(request)
-#        self.assertEqual(info.status_int, 500)
+def test_neighborhood_returns_correctly(sql_session):
+    neighborhood = Neighborhoods_Model.neighborhood(47.6770046, -122.3849916,
+                                                    sql_session)
+    assert neighborhood == 'Loyal Heights'
